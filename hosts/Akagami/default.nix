3144f31f0b67e447f559a90432be58e7fcbadc40
rxyhn <rxyhn@proton.me>
2022-11-22 14:50:12 +0700

    feat: picom

--- hosts/Akagami/default.nix
+++ hosts/Akagami/default.nix
@@ -22,47 +22,96 @@
     ];
   };
 
-  nixpkgs.overlays = [inputs.nixpkgs-f2k.overlays.window-managers];
+  services = {
+    xserver = {
+      enable = true;
 
-  services.xserver = {
-    enable = true;
-
-    displayManager = {
-      autoLogin = {
-        enable = true;
-        user = "rxyhn";
+      displayManager = {
+        autoLogin = {
+          enable = true;
+          user = "rxyhn";
+        };
+        defaultSession = "none+awesome";
+        lightdm = {
+          enable = true;
+        };
       };
 
-      defaultSession = "none+awesome";
+      dpi = 192;
+      exportConfiguration = true;
+      layout = "us";
 
-      lightdm = {
+      libinput = {
         enable = true;
+        touchpad = {
+          accelSpeed = "0.4";
+          naturalScrolling = true;
+        };
+      };
+
+      windowManager = {
+        awesome = {
+          enable = true;
+          package = pkgs.awesome-git;
+          luaModules = with pkgs.lua52Packages; [
+            lgi
+            ldbus
+            luarocks-nix
+            luadbi-mysql
+            luaposix
+          ];
+        };
       };
     };
 
-    dpi = 216;
-    exportConfiguration = true;
-    layout = "us";
-
-    libinput = {
+    picom = {
       enable = true;
+      backend = "glx";
+      vSync = true;
+      shadow = true;
+      shadowOffsets = [(-40) (-40)];
+      shadowOpacity = 0.40;
 
-      touchpad = {
-        accelSpeed = "0.4";
-        naturalScrolling = true;
-      };
-    };
-
-    windowManager.awesome = {
-      enable = true;
-      package = pkgs.awesome-git;
-      luaModules = with pkgs.lua52Packages; [
-        lgi
-        ldbus
-        luarocks-nix
-        luadbi-mysql
-        luaposix
+      shadowExclude = [
+        "class_g = 'slop'"
+        "window_type = 'menu'"
+        "window_type = 'dock'"
+        "window_type = 'desktop'"
+        "class_g = 'Firefox' && window_type *= 'utility'"
+        "_GTK_FRAME_EXTENTS@:c"
       ];
+
+      opacityRules = [
+        "70:class_g = 'splash'"
+      ];
+
+      wintypes = {
+        popup_menu = {full-shadow = true;};
+        dropdown_menu = {full-shadow = true;};
+        notification = {full-shadow = true;};
+        normal = {full-shadow = true;};
+      };
+
+      settings = {
+        shadow-radius = 40;
+        use-damage = true;
+
+        corner-radius = 8;
+        rounded-corners-exclude = [
+          "!window_type = 'normal' && !window_type = 'dialog'"
+        ];
+
+        blur-method = "dual_kawase";
+        blur-strength = 7.0;
+        kernel = "11x11gaussian";
+        blur-background = false;
+        blur-background-frame = true;
+        blur-background-fixed = true;
+
+        blur-background-exclude = [
+          "!window_type = 'splash'"
+        ];
+      };
     };
   };
 
