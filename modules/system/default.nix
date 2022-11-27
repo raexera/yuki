5f392a0940e1d8351db867eaf2c1a40bb368e794
rxyhn <rxyhn.dev@gmail.com>
2022-11-28 06:49:27 +0700

    theme: fix console colors

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -20,8 +20,8 @@ in {
   };
 
   console = let
-    normal = with theme.xcolors; [base red green yellow blue mauve teal subtext1];
-    bright = with theme.xcolors; [surface0 red green yellow blue mauve teal subtext0];
+    normal = with theme.colors; [base red green yellow blue mauve teal subtext1];
+    bright = with theme.colors; [surface0 red green yellow blue mauve teal subtext0];
   in {
     colors = normal ++ bright;
     font = "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
