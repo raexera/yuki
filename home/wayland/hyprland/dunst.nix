e0b84e3c37c67eb26c433c011dac17d08f1043cb
rxyhn <rxyhn.dev@gmail.com>
2022-11-27 13:01:31 +0700

    wayland: notification daemon update

--- home/wayland/hyprland/dunst.nix
+++ home/wayland/hyprland/dunst.nix
@@ -7,52 +7,58 @@
 in {
   services.dunst = {
     enable = true;
-    package = pkgs.fetchFromGitHub {
-      owner = "k-vernooy";
-      repo = "dunst";
-      rev = "61567d58855ba872f8237861ddcd786d03dd2631";
-      sha256 = "ttaaomjb3CclZG9JPdzDBj5XXlqRaKGPBY3ahFofqVM=";
-    };
     iconTheme = {
       package = pkgs.catppuccin-folders;
       name = "Papirus";
     };
     settings = {
       global = {
-        frame_color = "#${theme.colors.mauve}";
-        separator_color = "#${theme.colors.blue}";
         width = 220;
         height = 220;
-        offset = "0x15";
-        font = "JetBrainsMono Nerd Font 16";
-        corner_radius = 10;
-        origin = "top-center";
+        padding = 10;
+        frame_width = 3;
+        progress_bar_frame_width = 2;
+        corner_radius = 12;
+        transparency = 10;
         notification_limit = 3;
         idle_threshold = 120;
-        ignore_newline = "no";
-        mouse_left_click = "close_current";
-        mouse_right_click = "close_all";
-        sticky_history = "yes";
         history_length = 20;
         show_age_threshold = 60;
+        max_icon_size = 64;
+        separator_height = 1;
+        alignment = "center";
         ellipsize = "middle";
-        padding = 10;
-        always_run_script = true;
-        frame_width = 3;
-        transparency = 10;
-        progress_bar = true;
-        progress_bar_frame_width = 2;
-        format = "<b>%a</b>\n%s";
+        origin = "top-center";
+        offset = "0x15";
+        icon_position = "left";
+        font = "JetBrainsMono Nerd Font 13";
         highlight = "#${theme.colors.mauve}";
+        separator_color = "auto";
+        ignore_newline = "no";
+        mouse_left_click = "do_action";
+        mouse_middle_click = "close_all";
+        mouse_right_click = "close_current";
+        markup = "yes";
+        sticky_history = "yes";
+        always_run_script = true;
+        progress_bar = true;
+        format = "<b>%a</b>\n%s";
+        shrink = "no";
+        word_wrap = "yes";
       };
+
+      fullscreen_delay_everything = {fullscreen = "delay";};
+
       urgency_low = {
         background = "#${theme.colors.base}";
         foreground = "#${theme.colors.text}";
+        frame_color = "#${theme.colors.mauve}";
         timeout = 5;
       };
       urgency_normal = {
         background = "#${theme.colors.base}";
         foreground = "#${theme.colors.text}";
+        frame_color = "#${theme.colors.mauve}";
         timeout = 6;
       };
       urgency_critical = {
