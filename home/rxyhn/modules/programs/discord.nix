5b8106208fbc01dbeb6ec9fc7434ebbc0a1c1acb
Mihai Fufezan <fufexan@protonmail.com>
2022-12-11 17:35:52 +0200

    modules/programs/discord: update override

--- home/rxyhn/modules/programs/discord.nix
+++ home/rxyhn/modules/programs/discord.nix
@@ -1,16 +1,61 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
   home.packages = with pkgs; [
-    ((discord-canary.override {
+    (let
+      binaryName = "DiscordCanary";
+
+      disableBreakingUpdates =
+        runCommand "disable-breaking-updates.py"
+        {
+          pythonInterpreter = "${python3.interpreter}";
+          configDirName = lib.toLower binaryName;
+        } ''
+          mkdir -p $out/bin
+          cp "${inputs.nixpkgs}/pkgs/applications/networking/instant-messengers/discord/disable-breaking-updates.py" $out/bin/disable-breaking-updates.py
+          substituteAllInPlace $out/bin/disable-breaking-updates.py
+          chmod +x $out/bin/disable-breaking-updates.py
+        '';
+    in ((discord-canary.override {
         nss = pkgs.nss_latest;
         withOpenASAR = true;
       })
-      .overrideAttrs (old: {
-        libPath = old.libPath + ":${pkgs.libglvnd}/lib";
-        nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.makeWrapper];
+      .overrideAttrs (old: rec {
+        libPath = old.libPath + ":${libglvnd}/lib";
 
-        postFixup = ''
-          wrapProgram $out/opt/DiscordCanary/DiscordCanary --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland}}"
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/{bin,opt/${binaryName},share/pixmaps,share/icons/hicolor/256x256/apps}
+          mv * $out/opt/${binaryName}
+
+          chmod +x $out/opt/${binaryName}/${binaryName}
+          patchelf --set-interpreter ${stdenv.cc.bintools.dynamicLinker} \
+              $out/opt/${binaryName}/${binaryName}
+
+          wrapProgramShell $out/opt/${binaryName}/${binaryName} \
+              "''${gappsWrapperArgs[@]}" \
+              --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations}}" \
+              --prefix XDG_DATA_DIRS : "${gtk3}/share/gsettings-schemas/${gtk3.name}/" \
+              --prefix LD_LIBRARY_PATH : ${libPath}:$out/opt/${binaryName} \
+              --run "${lib.getExe disableBreakingUpdates}"
+
+          ln -s $out/opt/${binaryName}/${binaryName} $out/bin/
+          # Without || true the install would fail on case-insensitive filesystems
+          ln -s $out/opt/${binaryName}/${binaryName} $out/bin/${
+            lib.strings.toLower binaryName
+          } || true
+
+          ln -s $out/opt/${binaryName}/discord.png $out/share/pixmaps/${old.pname}.png
+          ln -s $out/opt/${binaryName}/discord.png $out/share/icons/hicolor/256x256/apps/${old.pname}.png
+
+          ln -s "${old.desktopItem}/share/applications" $out/share/
+
+          runHook postInstall
         '';
-      }))
+      })))
   ];
 }
