cb6066d58d95a8cb4a0199f7932139efeb0cc6dd
rxyhn <rxyhn.dev@gmail.com>
2022-12-12 11:50:48 +0700

    home/rxyhn/modules/programs [kitty] update colors

--- home/rxyhn/modules/programs/kitty.nix
+++ home/rxyhn/modules/programs/kitty.nix
@@ -8,6 +8,7 @@ in {
   programs.kitty = {
     enable = true;
     settings = {
+      # General
       background_opacity = "1.0";
       font_family = "Liga SFMono Nerd Font";
       font_size = 13;
@@ -32,50 +33,42 @@ in {
       window_padding_width = 15;
       window_margin_width = 10;
 
-      # The basic colors
+      # Colorschemes
       foreground = "#${colors.base05}";
       background = "#${colors.base00}";
       selection_background = "#${colors.base05}";
       selection_foreground = "#${colors.base00}";
-
-      # Cursor colors
-      cursor = "#${colors.base04}";
-      cursor_text_color = "#${colors.base00}";
-
-      # URL underline color when hovering with mouse
       url_color = "#${colors.base04}";
-
-      # Kitty window border colors
+      cursor = "#${colors.base05}";
       active_border_color = "#${colors.base03}";
       inactive_border_color = "#${colors.base01}";
-
-      # Tab bar colors
       active_tab_background = "#${colors.base00}";
-      active_tab_foreground = "#${colors.base06}";
+      active_tab_foreground = "#${colors.base05}";
       inactive_tab_background = "#${colors.base01}";
       inactive_tab_foreground = "#${colors.base04}";
       tab_bar_background = "#${colors.base01}";
-
-      # Base16 colors
-      # Normal
       color0 = "#${colors.base00}";
-      color1 = "#${colors.base0B}";
-      color2 = "#${colors.base0C}";
-      color3 = "#${colors.base0D}";
-      color4 = "#${colors.base07}";
-      color5 = "#${colors.base0F}";
-      color6 = "#${colors.base09}";
-      color7 = "#${colors.base04}";
-
-      # Bright
+      color1 = "#${colors.base08}";
+      color2 = "#${colors.base0B}";
+      color3 = "#${colors.base0A}";
+      color4 = "#${colors.base0D}";
+      color5 = "#${colors.base0E}";
+      color6 = "#${colors.base0C}";
+      color7 = "#${colors.base05}";
       color8 = "#${colors.base03}";
-      color9 = "#${colors.base0B}";
-      color10 = "#${colors.base0C}";
-      color11 = "#${colors.base0D}";
-      color12 = "#${colors.base07}";
-      color13 = "#${colors.base0F}";
-      color14 = "#${colors.base09}";
-      color15 = "#${colors.base06}";
+      color9 = "#${colors.base08}";
+      color10 = "#${colors.base0B}";
+      color11 = "#${colors.base0A}";
+      color12 = "#${colors.base0D}";
+      color13 = "#${colors.base0E}";
+      color14 = "#${colors.base0C}";
+      color15 = "#${colors.base07}";
+      color16 = "#${colors.base09}";
+      color17 = "#${colors.base0F}";
+      color18 = "#${colors.base01}";
+      color19 = "#${colors.base02}";
+      color20 = "#${colors.base04}";
+      color21 = "#${colors.base06}";
     };
 
     keybindings = {
