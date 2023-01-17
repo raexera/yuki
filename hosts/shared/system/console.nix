6d9043ab0f9ce682842843b04cd078c8f73c73c6
rxyhn <rxyhn.dev@gmail.com>
2023-01-18 03:31:05 +0700

    fix: console colors

--- hosts/shared/system/console.nix
+++ hosts/shared/system/console.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   console = let
-    normal = ["45475A" "F38BA8" "A6E3A1" "F9E2AF" "89B4FA" "F5C2E7" "94E2D5" "BAC2DE"];
-    bright = ["585B70" "F38BA8" "A6E3A1" "F9E2AF" "89B4FA" "F5C2E7" "94E2D5" "A6ADC8"];
+    normal = ["1E1E2E" "F38BA8" "A6E3A1" "F9E2AF" "89B4FA" "F5C2E7" "94E2D5" "BAC2DE"];
+    bright = ["313244" "F38BA8" "A6E3A1" "F9E2AF" "89B4FA" "F5C2E7" "94E2D5" "A6ADC8"];
   in {
     colors = normal ++ bright;
     font = "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
