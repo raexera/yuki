23470a62d460b1eed61e53d0e8e469f031f563c0
rxyhn <rxyhn.dev@gmail.com>
2023-01-26 09:49:47 +0700

    [hosts/lenovo]: update tlp settings

--- hosts/lenovo/default.nix
+++ hosts/lenovo/default.nix
@@ -79,7 +79,11 @@
       enable = true;
       settings = {
         START_CHARGE_THRESH_BAT0 = 0;
-        STOP_CHARGE_THRESH_BAT0 = 80;
+        STOP_CHARGE_THRESH_BAT0 = 1; # battery conservation mode
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       };
     };
   };
