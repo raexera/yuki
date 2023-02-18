f2f281f4b503da0d0021d592962cda5db08b5147
rxyhn <rxyhn.dev@gmail.com>
2023-02-18 10:31:12 +0700

    [chore]: updates

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,5 +1,3 @@
-# Add your reusable NixOS modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
-# These should be stuff you would like to share with others, not your personal configurations.
 {
-  awesome = import ./windowManager/awesome.nix;
+  awesome = import ./awesome.nix;
 }
