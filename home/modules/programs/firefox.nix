ea99b6b3eb371642429483cd82c41670207feec7
rxyhn <rxyhn.dev@gmail.com>
2024-01-01 22:47:49 +0700

    [chore}: update

--- home/modules/programs/firefox.nix
+++ home/modules/programs/firefox.nix
@@ -11,9 +11,14 @@ in {
     enable = true;
     profiles.rxyhn = {
       settings = {
-        "media.ffmpeg.vaapi.enabled" = true;
-        "media.rdd-ffmpeg.enabled" = true;
+        "gfx.canvas.accelerated" = true;
+        "gfx.webrender.enabled" = true;
         "gfx.x11-egl.force-enabled" = true;
+        "layers.acceleration.force-enabled" = true;
+        "media.av1.enabled" = false;
+        "media.ffmpeg.vaapi.enabled" = true;
+        "media.hardware-video-decoding.force-enabled" = true;
+        "media.rdd-ffmpeg.enabled" = true;
         "widget.dmabuf.force-enabled" = true;
       };
       userChrome = ''
