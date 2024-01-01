ace1fdad1e7cc4c21ed5ae964388f30927440917
rxyhn <rxyhn.dev@gmail.com>
2024-01-02 00:13:17 +0700

    [chore}: update

--- home/modules/programs/git.nix
+++ home/modules/programs/git.nix
@@ -11,15 +11,47 @@
 
   programs.git = {
     enable = true;
+    lfs.enable = true;
+    delta.enable = true;
+
     userName = "rxyhn";
     userEmail = "rxyhn.dev@gmail.com";
     signing = {
       key = "A69A110979DF4E36";
       signByDefault = true;
     };
+
+    aliases = {
+      a = "add";
+      af = "!git add $(git ls-files -m -o --exclude-standard | sk -m)";
+      b = "branch";
+      br = "branch";
+      c = "commit";
+      ca = "commit --amend";
+      cm = "commit -m";
+      co = "checkout";
+      d = "diff";
+      ds = "diff --staged";
+      edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; hx `f`";
+      essa = "push --force";
+      fuck = "commit --amend -m";
+      graph = "log --all --decorate --graph --oneline";
+      hist = "log --pretty=format:\"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)\" --graph --date=relative --decorate --all";
+      l = "log";
+      llog = "log --graph --name-status --pretty=format:\"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset\" --date=relative";
+      oops = "checkout --";
+      p = "push";
+      pf = "push --force-with-lease";
+      pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
+      ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
+      r = "rebase";
+      s = "status --short";
+      ss = "status";
+      st = "status";
+    };
+
     extraConfig = {
       init = {defaultBranch = "main";};
-      delta = {line-numbers = true;};
       branch.autosetupmerge = "true";
       push.default = "current";
       merge.stat = "true";
@@ -31,38 +63,17 @@
         autoStash = true;
       };
       rerere = {
-        autoupdate = true;
         enabled = true;
+        autoupdate = true;
       };
     };
-    lfs.enable = true;
-    delta.enable = true;
-    ignores = ["*~" "*.swp" "*result*" ".direnv" "node_modules"];
-    aliases = {
-      essa = "push --force";
-      co = "checkout";
-      fuck = "commit --amend -m";
-      c = "commit -m";
-      ca = "commit -am";
-      forgor = "commit --amend --no-edit";
-      graph = "log --all --decorate --graph --oneline";
-      oops = "checkout --";
-      l = "log";
-      r = "rebase";
-      s = "status --short";
-      ss = "status";
-      d = "diff";
-      ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
-      pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
-      af = "!git add $(git ls-files -m -o --exclude-standard | sk -m)";
-      st = "status";
-      br = "branch";
-      df = "!git hist | peco | awk '{print $2}' | xargs -I {} git diff {}^ {}";
-      hist = ''
-        log --pretty=format:"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)" --graph --date=relative --decorate --all'';
-      llog = ''
-        log --graph --name-status --pretty=format:"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset" --date=relative'';
-      edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; hx `f`";
-    };
+
+    ignores = [
+      "*~"
+      "*.swp"
+      "*result*"
+      ".direnv"
+      "node_modules"
+    ];
   };
 }
