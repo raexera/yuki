089dee50f173b69637d46165fc8fd8191bbe9ee5
rxyhn <rxyhn.dev@gmail.com>
2024-01-04 05:43:57 +0700

    [chore]: minor update

--- home/modules/windowManager/hyprland/config/default.nix
+++ home/modules/windowManager/hyprland/config/default.nix
@@ -47,17 +47,17 @@
       bezier = [
         "smoothOut, 0.36, 0, 0.66, -0.56"
         "smoothIn, 0.25, 1, 0.5, 1"
-        "overshot, 0.4,0.8,0.2,1.2"
+        "overshot, 0.4, 0.8, 0.2, 1.2"
       ];
 
       animation = [
         "windows, 1, 4, overshot, slide"
         "windowsOut, 1, 4, smoothOut, slide"
-        "border,1,10,default"
+        "border, 1, 10, default"
 
         "fade, 1, 10, smoothIn"
         "fadeDim, 1, 10, smoothIn"
-        "workspaces,1,4,overshot,slidevert"
+        "workspaces,1, 4, overshot,slide"
       ];
     };
 
--- home/modules/windowManager/hyprland/config/rules.nix
+++ home/modules/windowManager/hyprland/config/rules.nix
@@ -13,18 +13,17 @@
       "ignorezero, lockscreen"
       "ignorezero, notifications"
       "ignorezero, wofi"
-      "noanim, launcher"
-      "noanim, wofi"
     ];
 
     windowrulev2 = [
       "float, class:^(imv)$"
+      "float, class:^(wofi)$"
+      "float, title:^(com.github.Aylur.ags)$"
       "float, title:^(Media viewer)$"
       "float, title:^(Picture-in-Picture)$"
-      "float, title:^(com.github.Aylur.ags)$"
+      "idleinhibit fullscreen, class:^(firefox)$"
       "idleinhibit focus, class:^(firefox)$, title:^(.*YouTube.*)$"
       "idleinhibit focus, class:^(mpv)$"
-      "idleinhibit fullscreen, class:^(firefox)$"
       "pin, title:^(Picture-in-Picture)$"
       "workspace special silent, title:^(.*is sharing (your screen|a window)\.)$"
       "workspace special silent, title:^(Firefox — Sharing Indicator)$"
