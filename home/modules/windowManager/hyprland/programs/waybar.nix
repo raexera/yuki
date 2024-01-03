f1f95e6cc9f38cf137cbae97576b6ef89089ad09
rxyhn <rxyhn.dev@gmail.com>
2024-01-04 00:31:06 +0700

    [chore]: update

--- home/modules/windowManager/hyprland/programs/waybar.nix
+++ home/modules/windowManager/hyprland/programs/waybar.nix
@@ -22,7 +22,6 @@ in {
         passthrough = false;
         fixed-center = true;
         gtk-layer-shell = true;
-        height = 40;
         spacing = 0;
         margin-top = 0;
         margin-right = 0;
@@ -30,8 +29,8 @@ in {
         margin-left = 0;
         modules-left = [
           "custom/search"
+          "user"
           "hyprland/workspaces"
-          "hyprland/window"
         ];
         modules-center = [];
         modules-right = [
@@ -42,16 +41,16 @@ in {
           "battery"
           "clock#date"
           "clock"
-          "custom/power"
+          "group/group-power"
         ];
         "custom/search" = {
           format = " ";
           tooltip = false;
           on-click = "sh -c 'run-as-service $(wofi -S drun)'";
         };
-        "hyprland/window" = {
-          format = "{class}";
-          separate-outputs = false;
+        user = {
+          format = "{user}";
+          icon = false;
         };
         "hyprland/workspaces" = {
           active-only = false;
@@ -77,7 +76,6 @@ in {
           format = "{icon} {volume}%";
           format-muted = "󰖁";
           format-icons = {default = ["󰕿" "󰖀" "󰕾"];};
-          tooltip-format = "{desc}, {volume}%";
           on-click = "${_ pamixer} -t";
           on-scroll-up = "${_ pamixer} -d 1";
           on-scroll-down = "${_ pamixer} -i 1";
@@ -123,14 +121,45 @@ in {
           ];
         };
         clock = {
-          tooltip = false;
-          format = "󱑎 {:%H:%M}";
+          format = "󱑎 {:%I:%M %p}";
+          format-alt = "󱑎 {:%H:%M}";
         };
         "clock#date" = {
           format = "󰃶 {:%a %d %b}";
           tooltip-format = ''
             <big>{:%Y %B}</big>
             <tt><small>{calendar}</small></tt>'';
+        };
+        "group/group-power" = {
+          orientation = "inherit";
+          drawer = {
+            "transition-duration" = 500;
+            "children-class" = "not-power";
+            "transition-left-to-right" = false;
+          };
+          modules = [
+            "custom/power"
+            "custom/quit"
+            "custom/lock"
+            "custom/suspend"
+            "custom/reboot"
+          ];
+        };
+        "custom/quit" = {
+          format = "󰍃";
+          tooltip = false;
+        };
+        "custom/lock" = {
+          format = "󰌾";
+          tooltip = false;
+        };
+        "custom/suspend" = {
+          format = "󰒲";
+          tooltip = false;
+        };
+        "custom/reboot" = {
+          format = "󰜉";
+          tooltip = false;
         };
         "custom/power" = {
           format = "󰐥";
@@ -142,7 +171,13 @@ in {
     style = ''
       @define-color Background #000000;
       @define-color Foreground #ffffff;
-      @define-color Accent #00FFFF;
+      @define-color Accent #88C0D0;
+      @define-color Red #BF616A;
+      @define-color Orange #D08770;
+      @define-color Yellow #EBCB8B;
+      @define-color Green #A3BE8C;
+      @define-color Blue #81A1C1;
+      @define-color Magenta #B48EAD;
 
       * {
         all: initial;
@@ -151,21 +186,17 @@ in {
         min-height: 0;
         min-width: 0;
         font-family: "Material Design Icons", monospace;
-        font-size: 11pt;
+        font-size: 1rem;
       }
 
       window#waybar {
         background-color: alpha(@Background, 0.2);
       }
 
-      window#waybar.hidden {
-        opacity: 0;
-      }
-
       #custom-search {
         margin: 0 0.41em;
         padding: 0.41em 0.82em;
-        background-image: url("${snowflake}");
+        background-image: url("/nix/store/488hqs4jj4i981k6pfbbmxd62nigkpxa-Logo-14mbpw8jv1w2c5wvfvj8clmjw0fi956bq5xf9s2q3my14far0as8.svg");
         background-size: 80%;
         background-position: center;
         background-repeat: no-repeat;
@@ -180,6 +211,11 @@ in {
 
       #workspaces button {
         margin: 0 0.82em;
+      }
+
+      #workspaces button:hover {
+        box-shadow: inherit;
+        text-shadow: inherit;
       }
 
       #workspaces button label {
@@ -200,6 +236,7 @@ in {
           0 0 0.55em @Yellow, 0 0 0.68em @Yellow;
       }
 
+      #user,
       #workspaces button.active label {
         color: alpha(@Accent, 0.8);
         text-shadow: 0 0 0.14em @Accent, 0 0 0.27em @Accent, 0 0 0.41em @Accent,
@@ -210,12 +247,16 @@ in {
       #battery,
       #clock,
       #clock.date,
+      #custom-lock,
       #custom-power,
+      #custom-reboot,
+      #custom-suspend,
+      #custom-quit,
       #network,
       #pulseaudio,
       #pulseaudio.microphone,
       #tray,
-      #window {
+      #user {
         color: alpha(@Foreground, 0.8);
         background-color: alpha(@Background, 0.2);
         border-radius: 4px;
@@ -223,8 +264,24 @@ in {
         padding: 0.41em 0.82em;
       }
 
+      #custom-quit {
+        color: alpha(@Magenta, 0.8);
+      }
+
+      #custom-lock {
+        color: alpha(@Green, 0.8);
+      }
+
+      #custom-suspend {
+        color: alpha(@Blue, 0.8);
+      }
+
+      #custom-reboot {
+        color: alpha(@Yellow, 0.8);
+      }
+
       #custom-power {
-        margin-right: 0.41em;
+        color: alpha(@Red, 0.8);
       }
 
       tooltip {
