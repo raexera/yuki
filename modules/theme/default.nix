15f9e9440168fade9edecc7ad9fd54e1c9e4bd74
rxyhn <rxyhn.dev@gmail.com>
2024-01-07 00:25:35 +0700

    [chore]: update wallpaper path

--- modules/theme/default.nix
+++ modules/theme/default.nix
@@ -8,7 +8,7 @@
 
   wallpaper = builtins.fetchurl rec {
     name = "wallpaper-${sha256}.png";
-    url = "https://media.githubusercontent.com/media/rxyhn/wallpapers/main/anime/wallpaper.png";
+    url = "https://media.githubusercontent.com/media/rxyhn/wallpapers/main/anime/nord.png";
     sha256 = "0jbnr6qhqdwki0rzbq4x41car77mpdfpb0p2v7yfwl15m4f1ibkm";
   };
 }
