fd6e75cf5ab5407bbddd18aaef4cf11fa541fef0
rxyhn <rxyhn.dev@gmail.com>
2024-01-11 01:42:19 +0700

    [chore]: shell updates

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,75 +1,93 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    zsh-forgit
+    act
+    gist
     gitflow
-    gh
+    zsh-forgit
   ];
 
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
-    delta.enable = true;
-
-    userName = "rxyhn";
-    userEmail = "rxyhn.dev@gmail.com";
-    signing = {
-      key = "A69A110979DF4E36";
-      signByDefault = true;
-    };
-
-    aliases = {
-      a = "add";
-      af = "!git add $(git ls-files -m -o --exclude-standard | sk -m)";
-      b = "branch";
-      br = "branch";
-      c = "commit";
-      ca = "commit --amend";
-      cm = "commit -m";
-      co = "checkout";
-      d = "diff";
-      ds = "diff --staged";
-      edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; hx `f`";
-      essa = "push --force";
-      fuck = "commit --amend -m";
-      graph = "log --all --decorate --graph --oneline";
-      hist = "log --pretty=format:\"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)\" --graph --date=relative --decorate --all";
-      l = "log";
-      llog = "log --graph --name-status --pretty=format:\"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset\" --date=relative";
-      oops = "checkout --";
-      p = "push";
-      pf = "push --force-with-lease";
-      pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
-      ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
-      r = "rebase";
-      s = "status --short";
-      ss = "status";
-      st = "status";
-    };
-
-    extraConfig = {
-      init = {defaultBranch = "main";};
-      branch.autosetupmerge = "true";
-      push.default = "current";
-      merge.stat = "true";
-      core.whitespace = "fix,-indent-with-non-tab,trailing-space,cr-at-eol";
-      repack.usedeltabaseoffset = "true";
-      pull.ff = "only";
-      rebase = {
-        autoSquash = true;
-        autoStash = true;
-      };
-      rerere = {
-        enabled = true;
-        autoupdate = true;
+  programs = {
+    gh = {
+      enable = true;
+      gitCredentialHelper.enable = false;
+      extensions = with pkgs; [
+        gh-dash
+        gh-eco
+        gh-cal
+      ];
+      settings = {
+        git_protocol = "ssh";
+        prompt = "enabled";
       };
     };
 
-    ignores = [
-      "*~"
-      "*.swp"
-      "*result*"
-      ".direnv"
-      "node_modules"
-    ];
+    git = {
+      enable = true;
+      package = pkgs.gitAndTools.gitFull;
+      lfs.enable = true;
+      delta.enable = true;
+
+      userName = "rxyhn";
+      userEmail = "rxyhn.dev@gmail.com";
+      signing = {
+        key = "A69A110979DF4E36";
+        signByDefault = true;
+      };
+
+      aliases = {
+        a = "add";
+        af = "!git add $(git ls-files -m -o --exclude-standard | sk -m)";
+        b = "branch";
+        br = "branch";
+        c = "commit";
+        ca = "commit --amend";
+        cm = "commit -m";
+        co = "checkout";
+        d = "diff";
+        ds = "diff --staged";
+        edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; hx `f`";
+        essa = "push --force";
+        fuck = "commit --amend -m";
+        graph = "log --all --decorate --graph --oneline";
+        hist = "log --pretty=format:\"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)\" --graph --date=relative --decorate --all";
+        l = "log";
+        llog = "log --graph --name-status --pretty=format:\"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset\" --date=relative";
+        oops = "checkout --";
+        p = "push";
+        pf = "push --force-with-lease";
+        pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
+        ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
+        r = "rebase";
+        s = "status --short";
+        ss = "status";
+        st = "status";
+      };
+
+      extraConfig = {
+        init.defaultBranch = "main";
+        branch.autosetupmerge = "true";
+        push.default = "current";
+        merge.stat = "true";
+        core.whitespace = "fix,-indent-with-non-tab,trailing-space,cr-at-eol";
+        repack.usedeltabaseoffset = "true";
+        pull.ff = "only";
+        rebase = {
+          autoSquash = true;
+          autoStash = true;
+        };
+        rerere = {
+          enabled = true;
+          autoupdate = true;
+        };
+      };
+
+      ignores = [
+        "*~"
+        "*.swp"
+        "*result*"
+        ".direnv"
+        "node_modules"
+      ];
+    };
   };
 }
--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -16,31 +16,131 @@
       vids = "$HOME/Videos";
     };
 
-    autocd = true;
     enableAutosuggestions = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
 
     history = {
       expireDuplicatesFirst = true;
+      ignoreDups = true;
+      ignoreSpace = true;
       path = "${config.xdg.dataHome}/zsh_history";
     };
 
     sessionVariables = {
       LC_ALL = "en_US.UTF-8";
-      ZSH_AUTOSUGGEST_USE_ASYNC = "true";
     };
 
-    initExtra = ''
-      zstyle ':completion:*' completer _complete _ignored _approximate
-      zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
-      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-      zstyle ':completion:*' menu select
-      zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
-      zstyle ':completion:*' verbose true
+    completionInit = ''
+      # Load Zsh modules
+      autoload -U compinit
+      zmodload zsh/complist
+
+      # General completion behavior
+      zstyle ':completion:*' menu yes select # search
+      zstyle ':completion:*' sort false
       zstyle ':completion:*' use-cache on
       zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+      zstyle ':completion:*' verbose yes
+      zstyle ':completion:*' special-dirs true
+      zstyle ':completion:*' rehash true
+      zstyle ':completion:*' list-grouped false
+      zstyle ':completion:*' list-separator '''
+      zstyle ':completion:*' group-name '''
+
+      # Matcher and completer configurations
+      zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'l:|=* r:|=*'
+      zstyle ':completion:*' completer _complete _match _approximate
+      zstyle ':completion:*:match:*' original only
+      zstyle ':completion:*:approximate:*' max-errors 1 numeric
+
+      # Formatting and colors
+      zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+      zstyle ':completion:*:matches' group 'yes'
+      zstyle ':completion:*:warnings' format '%F{red}%B-- No match for: %d --%b%f'
+      zstyle ':completion:*:messages' format '%d'
+      zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
+      zstyle ':completion:*:descriptions' format '[%d]'
+
+      # Specific behaviors for types of completions
+      zstyle ':completion:complete:*:options' sort false
+      zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec))'
+      zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
+      zstyle ':completion:*:jobs' numbers true
+      zstyle ':completion:*:jobs' verbose true
+      zstyle ":completion:*:git-checkout:*" sort false
+      zstyle ':completion:*' file-sort modification
+      zstyle ':completion:*:eza' sort false
+      zstyle ':completion:files' sort false
+    '';
+
+    initExtra = ''
+      # Set options
+      while read -r option; do
+        setopt $option
+      done <<-EOF
+      AUTO_CD
+      AUTO_LIST
+      AUTO_MENU
+      AUTO_PARAM_SLASH
+      AUTO_PUSHD
+      APPEND_HISTORY
+      ALWAYS_TO_END
+      COMPLETE_IN_WORD
+      CORRECT
+      EXTENDED_HISTORY
+      HIST_EXPIRE_DUPS_FIRST
+      HIST_FCNTL_LOCK
+      HIST_IGNORE_ALL_DUPS
+      HIST_IGNORE_DUPS
+      HIST_IGNORE_SPACE
+      HIST_REDUCE_BLANKS
+      HIST_SAVE_NO_DUPS
+      HIST_VERIFY
+      INC_APPEND_HISTORY
+      INTERACTIVE_COMMENTS
+      MENU_COMPLETE
+      NO_NOMATCH
+      PUSHD_IGNORE_DUPS
+      PUSHD_TO_HOME
+      PUSHD_SILENT
+      SHARE_HISTORY
+      EOF
+
+      # Unset options
+      while read -r option; do
+        unsetopt $option
+      done <<-EOF
+      CORRECT_ALL
+      HIST_BEEP
+      MENU_COMPLETE
+      EOF
+
+      # Load Zsh modules
+      zmodload zsh/zle
+      zmodload zsh/zpty
+      zmodload zsh/complist
+
+      # Initialize completion system
       _comp_options+=(globdots)
+      compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-"$ZSH_VERSION"
+
+      # Colors configuration
+      autoload -Uz colors && colors
+
+      # Autosuggestion and syntax highlighting settings
+      ZSH_AUTOSUGGEST_USE_ASYNC="true"
+      ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor regexp root line)
+      ZSH_HIGHLIGHT_MAXLENGTH=512
+
+      # Vi mode key bindings
+      bindkey -v
+      bindkey -M menuselect 'h' vi-backward-char
+      bindkey -M menuselect 'k' vi-up-line-or-history
+      bindkey -M menuselect 'l' vi-forward-char
+      bindkey -M menuselect 'j' vi-down-line-or-history
+      bindkey "^A" vi-beginning-of-line
+      bindkey "^E" vi-end-of-line
     '';
 
     shellAliases = with lib;
@@ -71,21 +171,44 @@
       la = "${getExe eza} -lah --tree";
       ls = "${getExe eza} -h --git --icons --color=auto --group-directories-first -s extension";
       ps = getExe procs;
+      run = "nix run $@";
+      search = "nix search $@";
+      shell = "nix shell $@";
       tree = "${getExe eza} --tree --icons --tree";
       untar = "tar -xvf";
       untargz = "tar -xzf";
     };
 
-    plugins = [
+    plugins = with pkgs; [
+      {
+        name = "forgit";
+        file = "share/forgit/forgit.plugin.zsh";
+        src = fetchFromGitHub {
+          owner = "wfxr";
+          repo = "forgit";
+          rev = "d7f88d75b6cb3b2bd95f5a2351a3ee79d35a35f9";
+          sha256 = "sha256-WHhyllOr/PgR+vlrfMQs/3/d3xpmDylT6BlLCu50a2g=";
+        };
+      }
+      {
+        name = "zsh-autopair";
+        file = "zsh-autopair.plugin.zsh";
+        src = fetchFromGitHub {
+          owner = "hlissner";
+          repo = "zsh-autopair";
+          rev = "396c38a7468458ba29011f2ad4112e4fd35f78e6";
+          sha256 = "sha256-PXHxPxFeoYXYMOC29YQKDdMnqTO0toyA7eJTSCV6PGE=";
+        };
+      }
       {
         name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.8.0";
-          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-        };
+        src = zsh-nix-shell;
+        file = "share/zsh-nix-shell/nix-shell.plugin.zsh";
+      }
+      {
+        name = "zsh-vi-mode";
+        src = zsh-vi-mode;
+        file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
       }
     ];
   };
