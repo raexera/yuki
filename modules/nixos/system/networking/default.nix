b1a5a9bf0060adfb47c821c52865e94af0e4cf52
rxyhn <rxyhn.dev@gmail.com>
2024-01-12 12:22:18 +0700

    [chore]: updates

--- modules/nixos/system/networking/default.nix
+++ modules/nixos/system/networking/default.nix
@@ -5,13 +5,6 @@
   ];
 
   networking = {
-    extraHosts = builtins.readFile (
-      builtins.fetchurl {
-        url = "https://raw.githubusercontent.com/bebasid/bebasid/master/releases/hosts";
-        sha256 = "0993ms2lk7n3h82ccj2iabkaf9bzalrkmw9dg70jsfa7cc8309km";
-      }
-    );
-
     networkmanager = {
       enable = true;
       wifi.powersave = true;
