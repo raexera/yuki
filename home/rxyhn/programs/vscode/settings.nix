106c1b8f0210d808c3fc65f048698d0189067064
rxyhn <rxyhn.dev@gmail.com>
2024-01-28 22:49:37 +0700

    chore: vscode updates

--- home/rxyhn/programs/vscode/settings.nix
+++ home/rxyhn/programs/vscode/settings.nix
@@ -1,9 +1,5 @@
 {pkgs, ...}: let
   editor = {
-    "breadcrumbs.enabled" = true;
-    "breadcrumbs.symbolPath" = "last";
-    "editor.acceptSuggestionOnEnter" = "smart";
-    "editor.autoIndent" = "full";
     "editor.bracketPairColorization.enabled" = true;
     "editor.bracketPairColorization.independentColorPoolPerBracketType" = true;
     "editor.cursorBlinking" = "smooth";
@@ -15,23 +11,18 @@
     "editor.formatOnSave" = true;
     "editor.guides.bracketPairs" = true;
     "editor.guides.indentation" = true;
-    "editor.inlayHints.enabled" = "onUnlessPressed";
-    "editor.inlayHints.padding" = true;
     "editor.inlineSuggest.enabled" = true;
     "editor.linkedEditing" = true;
     "editor.lineHeight" = 22;
     "editor.lineNumbers" = "relative";
     "editor.minimap.enabled" = false;
-    "editor.minimap.renderCharacters" = false;
     "editor.renderLineHighlight" = "all";
-    "editor.renderWhitespace" = "none";
     "editor.semanticHighlighting.enabled" = true;
     "editor.showUnused" = true;
     "editor.smoothScrolling" = true;
     "editor.tabCompletion" = "on";
     "editor.tabSize" = 2;
     "editor.trimAutoWhitespace" = true;
-    "editor.wordWrap" = "on";
   };
 
   explorer = {
@@ -45,12 +36,7 @@
   };
 
   terminal = {
-    "terminal.integrated.cursorBlinking" = true;
-    "terminal.integrated.cursorStyle" = "line";
-    "terminal.integrated.cursorWidth" = 2;
-    "terminal.integrated.fontFamily" = "'Fira Code'";
     "terminal.integrated.fontSize" = 13;
-    "terminal.integrated.minimumContrastRatio" = 1;
     "terminal.integrated.smoothScrolling" = true;
   };
 
@@ -64,13 +50,10 @@
 
   workbench = {
     "workbench.colorTheme" = "Catppuccin Macchiato";
-    "workbench.editor.enablePreview" = false;
-    "workbench.editor.enablePreviewFromQuickOpen" = false;
     "workbench.iconTheme" = "catppuccin-macchiato";
     "workbench.layoutControl.enabled" = false;
     "workbench.panel.defaultLocation" = "right";
     "workbench.productIconTheme" = "icons-carbon";
-    "workbench.tree.renderIndentGuides" = "always";
     "workbench.sideBar.location" = "right";
     "workbench.startupEditor" = "none";
   };
@@ -93,7 +76,6 @@
   git = {
     "git.autofetch" = true;
     "git.enableSmartCommit" = true;
-    "gitlens.views.repositories.files.layout" = "tree";
   };
 
   github = {
@@ -101,7 +83,6 @@
       "*" = true;
     };
     "github.copilot.editor.enableAutoCompletions" = true;
-    "githubPullRequests.pullBranch" = "always";
   };
 
   path-intellisense = {
@@ -152,6 +133,7 @@ in {
   programs.vscode.userSettings =
     {
       "extensions.autoCheckUpdates" = false;
+      "extensions.ignoreRecommendations" = true;
       "update.mode" = "none";
     }
     // editor
