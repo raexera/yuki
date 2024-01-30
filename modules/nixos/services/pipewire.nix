7dd5b93d0035d041742e0011d139f5a7c9d07ea6
rxyhn <rxyhn.dev@gmail.com>
2024-01-30 18:07:54 +0700

    chore: deadnix

--- modules/nixos/services/pipewire.nix
+++ modules/nixos/services/pipewire.nix
@@ -4,13 +4,10 @@
 
   services.pipewire = {
     enable = true;
-    alsa = {
-      enable = true;
-      support32Bit = true;
-    };
-
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    jack.enable = true;
     pulse.enable = true;
     wireplumber.enable = true;
-    jack.enable = true;
   };
 }
