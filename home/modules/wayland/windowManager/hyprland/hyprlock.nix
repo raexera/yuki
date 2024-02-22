6f0b2d54df91eb7d51967629e0497cf9e236f255
rxyhn <rxyhn.dev@gmail.com>
2024-02-22 17:49:11 +0700

    chore: updates

--- home/modules/wayland/windowManager/hyprland/hyprlock.nix
+++ home/modules/wayland/windowManager/hyprland/hyprlock.nix
@@ -10,12 +10,22 @@
       disable_loading_bar = false;
       hide_cursor = true;
       grace = 2;
+      no_fade_in = false;
     };
 
     backgrounds = [
       {
         path = "";
         color = "rgba(0, 0, 0, 0.5)";
+
+        # Blur
+        blur_passes = 3; # 0 disables blurring
+        blur_size = 10;
+        noise = 0.0117;
+        contrast = 0.8916;
+        brightness = 0.8172;
+        vibrancy = 0.1696;
+        vibrancy_darkness = 0.0;
       }
     ];
 
@@ -37,7 +47,7 @@
         hide_input = false;
         position = {
           x = 0;
-          y = -30;
+          y = -90;
         };
         halign = "center";
         valign = "center";
@@ -48,24 +58,36 @@
       {
         text = "<b>$TIME</b>";
         color = "rgb(255, 255, 255)";
-        font_size = 72;
+        font_size = 96;
         position = {
           x = 0;
-          y = 250;
+          y = 0;
+        };
+        halign = "center";
+        valign = "top";
+      }
+      {
+        text = "Hi there, <i>$USER</i>!";
+        color = "rgb(255, 255, 255)";
+        font_size = 32;
+        position = {
+          x = 0;
+          y = 0;
         };
         halign = "center";
         valign = "center";
       }
       {
-        text = "Hi there, <i>$USER</i>!";
+        text = "";
         color = "rgb(255, 255, 255)";
-        font_size = 36;
+        font_family = "monospace";
+        font_size = 32;
         position = {
           x = 0;
-          y = 30;
+          y = 0;
         };
         halign = "center";
-        valign = "center";
+        valign = "bottom";
       }
     ];
   };
