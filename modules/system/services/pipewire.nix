ce9df8485f2b6d123638a9cd932343b83ece094b
rxyhn <rxyhn.dev@gmail.com>
2024-03-03 09:07:27 +0700

    chore: bump inputs

--- modules/system/services/pipewire.nix
+++ modules/system/services/pipewire.nix
@@ -1,4 +1,8 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   hardware.pulseaudio.enable = lib.mkForce false;
   security.rtkit.enable = true;
 
@@ -9,6 +13,19 @@
     audio.enable = true;
     jack.enable = true;
     pulse.enable = true;
-    wireplumber.enable = true;
+
+    wireplumber = {
+      enable = true;
+      configPackages = [
+        (pkgs.writeTextDir "share/bluetooth.lua.d/51-bluez-config.lua" ''
+          bluez_monitor.properties = {
+            ["bluez5.enable-sbc-xq"] = true,
+            ["bluez5.enable-msbc"] = true,
+            ["bluez5.enable-hw-volume"] = true,
+            ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+          }
+        '')
+      ];
+    };
   };
 }
