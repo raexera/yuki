1886a11bc5a73c06bdcd0ba328eb7a4835db8964
raexera <raexera@gmail.com>
2024-10-14 23:25:06 +0700

    chore: update env var

--- home/modules/wayland/windowManager/hyprland/default.nix
+++ home/modules/wayland/windowManager/hyprland/default.nix
@@ -116,6 +116,26 @@ in {
     lightctl
   ];
 
+  home.sessionVariables = {
+    # toolkit backend
+    GDK_BACKEND = "wayland,x11,*";
+    SDL_VIDEODRIVER = "wayland";
+    CLUTTER_BACKEND = "wayland";
+
+    # XDG
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+
+    # QT
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_QPA_PLATFORM = "wayland;xcb";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+
+    # firefox
+    MOZ_ENABLE_WAYLAND = "1";
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
 
--- hosts/profiles/hyprland/default.nix
+++ hosts/profiles/hyprland/default.nix
@@ -7,31 +7,6 @@
     ./greetd.nix
   ];
 
-  environment = {
-    variables = {
-      # aquamarine
-      AQ_DRM_DEVICES = "/dev/dri/card0:/dev/dri/card1";
-
-      # toolkit backend
-      GDK_BACKEND = "wayland,x11,*";
-      SDL_VIDEODRIVER = "wayland";
-      CLUTTER_BACKEND = "wayland";
-
-      # XDG
-      XDG_CURRENT_DESKTOP = "Hyprland";
-      XDG_SESSION_TYPE = "wayland";
-      XDG_SESSION_DESKTOP = "Hyprland";
-
-      # QT
-      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-      QT_QPA_PLATFORM = "wayland;xcb";
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-
-      # firefox
-      MOZ_ENABLE_WAYLAND = "1";
-    };
-  };
-
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.default;
