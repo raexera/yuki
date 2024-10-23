1d287ddb2a12332b41ae5008490ee4d0a9cf055d
raexera <raexera@gmail.com>
2024-10-23 19:37:30 +0700

    chore: update kitty

--- home/modules/programs/kitty.nix
+++ home/modules/programs/kitty.nix
@@ -7,37 +7,56 @@
       inherit (themes.colorscheme) xcolors;
       inherit (themes.colorscheme.xcolors) normal bright;
     in {
-      active_tab_font_style = "bold";
-      background_opacity = "0.8";
-      bold_font = "auto";
-      bold_italic_font = "auto";
-      confirm_os_window_close = 0;
-      cursor_blink_interval = "-1";
-      cursor_shape = "underline";
-      cursor_stop_blinking_after = 0;
-      cursor_underline_thickness = "1.5";
-      disable_ligatures = "never";
-      editor = "nvim";
+      # Fonts
       font_family = "JetBrains Mono";
-      font_size = "11.0";
-      hide_window_decorations = "titlebar-only";
-      inactive_tab_font_style = "normal";
-      inactive_text_alpha = "1.0";
+      bold_font = "auto";
       italic_font = "auto";
-      placement_strategy = "center";
-      resize_in_steps = "yes";
+      bold_italic_font = "auto";
+      disable_ligatures = "never";
+      font_size = "11.0";
+
+      # Cursor
+      cursor_shape = "underline";
+      cursor_underline_thickness = "1.5";
+      cursor_blink_interval = "-1";
+      cursor_stop_blinking_after = 0;
+
+      # Scrollback
       scrollback_lines = 10000;
+      touch_scroll_multiplier = "1.0";
+      wheel_scroll_multiplier = "5.0";
+
+      # Terminal bell
+      enable_audio_bell = "no";
+      visual_bell_duration = 0;
+      window_alert_on_bell = "no";
+      bell_on_tab = "no";
+      command_on_bell = "none";
+
+      # Window layout
+      remember_window_size = "no";
+      window_resize_step_cells = 1;
+      window_resize_step_lines = 1;
+      draw_minimal_borders = "yes";
+      hide_window_decorations = "yes";
+      inactive_text_alpha = "0.8";
+      placement_strategy = "center";
+      resize_in_steps = "no";
+      window_margin_width = 0;
+      window_padding_width = 15;
+      confirm_os_window_close = 0;
+
+      # Tab Bar
+      active_tab_font_style = "bold";
+      inactive_tab_font_style = "normal";
       tab_bar_edge = "bottom";
       tab_bar_min_tabs = 1;
       tab_bar_style = "powerline";
       tab_powerline_style = "slanted";
       tab_title_template = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
-      touch_scroll_multiplier = "1.0";
-      wheel_scroll_multiplier = "5.0";
-      window_margin_width = 0;
-      window_padding_width = 15;
 
-      # Basic colors
+      # Color scheme
+      background_opacity = "0.8";
       foreground = xcolors.foreground;
       background = xcolors.background;
       selection_foreground = xcolors.foreground;
@@ -107,14 +126,15 @@
     };
 
     keybindings = {
+      # Clipboard
+      "ctrl+shift+c" = "copy_to_clipboard";
       "ctrl+shift+v" = "paste_from_clipboard";
       "ctrl+shift+s" = "paste_from_selection";
-      "ctrl+shift+c" = "copy_to_clipboard";
-      "shift+insert" = "paste_from_selection";
 
+      # Scrolling
       "ctrl+shift+up" = "scroll_line_up";
-      "ctrl+shift+down" = "scroll_line_down";
       "ctrl+shift+k" = "scroll_line_up";
+      "ctrl+shift+down" = "scroll_line_down";
       "ctrl+shift+j" = "scroll_line_down";
       "ctrl+shift+page_up" = "scroll_page_up";
       "ctrl+shift+page_down" = "scroll_page_down";
@@ -122,6 +142,7 @@
       "ctrl+shift+end" = "scroll_end";
       "ctrl+shift+h" = "show_scrollback";
 
+      # Window management
       "ctrl+shift+enter" = "new_window";
       "ctrl+shift+n" = "new_os_window";
       "ctrl+shift+w" = "close_window";
@@ -130,6 +151,7 @@
       "ctrl+shift+f" = "move_window_forward";
       "ctrl+shift+b" = "move_window_backward";
       "ctrl+shift+`" = "move_window_to_top";
+      "ctrl+shift+r" = "start_resizing_window";
       "ctrl+shift+1" = "first_window";
       "ctrl+shift+2" = "second_window";
       "ctrl+shift+3" = "third_window";
@@ -141,19 +163,26 @@
       "ctrl+shift+9" = "ninth_window";
       "ctrl+shift+0" = "tenth_window";
 
+      # Tab management
       "ctrl+shift+right" = "next_tab";
       "ctrl+shift+left" = "previous_tab";
       "ctrl+shift+t" = "new_tab";
       "ctrl+shift+q" = "close_tab";
-      "ctrl+shift+l" = "next_layout";
       "ctrl+shift+." = "move_tab_forward";
       "ctrl+shift+," = "move_tab_backward";
       "ctrl+shift+alt+t" = "set_tab_title";
 
-      "ctrl+shift+equal" = "increase_font_size";
-      "ctrl+shift+minus" = "decrease_font_size";
-      "ctrl+shift+backspace" = "restore_font_size";
-      "ctrl+shift+f6" = "set_font_size 16.0";
+      # Layout management
+      "ctrl+shift+l" = "next_layout";
+      "ctrl+alt+t" = "goto_layout tall";
+      "ctrl+alt+s" = "goto_layout stack";
+      "ctrl+alt+p" = "last_used_layout";
+      "ctrl+alt+z" = "toggle_layout stack";
+
+      # Font sizes
+      "ctrl+shift+equal" = "change_font_size all +1.0";
+      "ctrl+shift+minus" = "change_font_size all -1.0";
+      "ctrl+shift+backspace" = "change_font_size all 0";
     };
   };
 }
