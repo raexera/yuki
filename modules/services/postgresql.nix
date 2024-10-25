4cf3f70367be9c38c50f40e2538229a23b995335
raexera <raexera@gmail.com>
2024-10-25 16:42:51 +0700

    chore: update inputs

--- modules/services/postgresql.nix
+++ modules/services/postgresql.nix
@@ -1,6 +1,21 @@
-{
+{pkgs, ...}: {
   services.postgresql = {
     enable = true;
     enableTCPIP = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+      #...
+      #type database DBuser origin-address auth-method
+      # ipv4
+      host  all      all     127.0.0.1/32   scram-sha-256
+      # ipv6
+      host all       all     ::1/128        scram-sha-256
+      # local
+      local all       all     trust
+    '';
+    initialScript = pkgs.writeText "backend-initScript" ''
+      CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
+      CREATE DATABASE nixcloud;
+      GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
+    '';
   };
 }
