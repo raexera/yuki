2af2a5900e5e625021404d0c65eb28e93ffd7743
raexera <raexera@gmail.com>
2024-10-31 04:21:29 +0700

    chore: update cursor size

--- home/modules/programs/gtk.nix
+++ home/modules/programs/gtk.nix
@@ -6,7 +6,7 @@
   home.pointerCursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Classic";
-    size = 16;
+    size = 24;
     gtk.enable = true;
     x11.enable = true;
   };
