1d69bfedc79bd581d8e6e9207b67a5e89818d958
raexera <raexera@gmail.com>
2024-11-13 05:43:06 +0700

    chore: cleanup qt

--- home/modules/config/qt.nix
+++ home/modules/config/qt.nix
@@ -18,13 +18,6 @@ in {
     style.name = "kvantum";
   };
 
-  home.packages = with pkgs; [
-    qt6Packages.qtstyleplugin-kvantum
-    qt6Packages.qt6ct
-    libsForQt5.qtstyleplugin-kvantum
-    libsForQt5.qt5ct
-  ];
-
   xdg.configFile = {
     "Kvantum" = {
       source = "${KvLibadwaita}/src";
