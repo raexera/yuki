a821ecec645428ead721e7ae398f0803a394b9c3
raexera <raexera@gmail.com>
2024-11-14 19:01:38 +0700

    chore: update rules

--- home/modules/services/window-managers/hyprland/config/rules.nix
+++ home/modules/services/window-managers/hyprland/config/rules.nix
@@ -5,6 +5,7 @@
       "dimaround, class:^(xdg-desktop-portal-gtk)$"
       "dimaround, class:^(polkit-gnome-authentication-agent-1)$"
 
+      "float, class:^(blueman-manager)$"
       "float, class:^(imv)$"
       "float, class:^(io.bassi.Amberol)$"
       "float, class:^(io.github.celluloid_player.Celluloid)$"
@@ -13,9 +14,10 @@
       "float, class:^(pavucontrol)$"
       "float, class:^(thunar)$"
       "float, class:^(xdg-desktop-portal-gtk)$"
-      "float, title:^(Media viewer)$"
       "float, title:^(Open Files?|All Files|Open Folder|Install from VSIX)$"
-      "float, title:^(Picture-in-Picture)$"
+      "float, title:^(Media viewer)$"
+      "float,title:(Authentication Required|Picture-in-Picture)"
+      "pin,title:(Authentication Required|Picture-in-Picture)"
 
       "idleinhibit focus, class:^(mpv|.+exe|celluloid)$"
       "idleinhibit focus, class:^(firefox)$, title:^(.*YouTube.*)$"
@@ -23,7 +25,18 @@
       "idleinhibit fullscreen, class:^(firefox)$"
       "idleinhibit fullscreen, class:^(zen)$"
 
-      "pin, title:^(Picture-in-Picture)$"
+      "bordersize 0, floating:0, onworkspace:w[t1]"
+      "rounding 0, floating:0, onworkspace:w[t1]"
+      "bordersize 0, floating:0, onworkspace:w[tg1]"
+      "rounding 0, floating:0, onworkspace:w[tg1]"
+      "bordersize 0, floating:0, onworkspace:f[1]"
+      "rounding 0, floating:0, onworkspace:f[1]"
+    ];
+
+    workspace = [
+      "w[t1], gapsout:0, gapsin:0"
+      "w[tg1], gapsout:0, gapsin:0"
+      "f[1], gapsout:0, gapsin:0"
     ];
   };
 }
