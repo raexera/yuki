5add4eed7ee9ba42152c3f839e2e2c4279dc2607
raexera <raexera@gmail.com>
2024-11-15 10:54:22 +0700

    chore: update firefox

--- home/modules/programs/firefox.nix
+++ home/modules/programs/firefox.nix
@@ -4,6 +4,9 @@
 
     profiles.raexera = {
       settings = {
+        "browser.tabs.inTitlebar" = 0;
+        "browser.urlbar.trimHttps" = true;
+        "browser.urlbar.trimURLs" = true;
         "gfx.canvas.accelerated" = true;
         "gfx.webrender.all" = true;
         "gfx.webrender.enabled" = true;
@@ -12,6 +15,10 @@
         "media.ffmpeg.vaapi.enabled" = true;
         "media.hardware-video-decoding.force-enabled" = true;
         "media.rdd-ffmpeg.enabled" = true;
+        "sidebar.animation.enabled" = true;
+        "sidebar.verticalTabs" = true;
+        "svg.context-properties.content.enabled" = true;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "widget.dmabuf.force-enabled" = true;
       };
     };
