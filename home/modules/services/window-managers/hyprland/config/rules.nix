5948dbaac4868df1b3ff813e7cf9cc45ca56349a
raexera <raexera@gmail.com>
2024-11-16 19:12:33 +0700

    chore: update hyprland settings

--- home/modules/services/window-managers/hyprland/config/rules.nix
+++ home/modules/services/window-managers/hyprland/config/rules.nix
@@ -25,17 +25,16 @@
       "idleinhibit fullscreen, title:^(*)$"
       "idleinhibit fullscreen, fullscreen:1"
 
-      "bordersize 0, floating:0, onworkspace:w[t1]"
-      "rounding 0, floating:0, onworkspace:w[t1]"
-      "bordersize 0, floating:0, onworkspace:w[tg1]"
-      "rounding 0, floating:0, onworkspace:w[tg1]"
+      "bordersize 0, floating:0, onworkspace:w[tv1]"
+      "rounding 0, floating:0, onworkspace:w[tv1]"
       "bordersize 0, floating:0, onworkspace:f[1]"
       "rounding 0, floating:0, onworkspace:f[1]"
+
+      "suppressevent maximize, class:.*"
     ];
 
     workspace = [
-      "w[t1], gapsout:0, gapsin:0"
-      "w[tg1], gapsout:0, gapsin:0"
+      "w[tv1], gapsout:0, gapsin:0"
       "f[1], gapsout:0, gapsin:0"
     ];
   };
