40a4cbcb337714d24f4e687e04972fa4e8b5df94
raexera <raexera@gmail.com>
2024-11-20 06:34:55 +0700

    chore: update zsh

--- home/modules/programs/zsh.nix
+++ home/modules/programs/zsh.nix
@@ -34,14 +34,12 @@
     };
 
     completionInit = ''
-      # Load zsh modules
       zmodload zsh/complist
-
-      # Enabling the Zsh Completion System
       autoload -U compinit; compinit
       _comp_options+=(globdots) # With hidden file-sort
+    '';
 
-      # Set Options
+    initExtra = ''
       while read -r option; do
         setopt $option
       done <<-EOF
@@ -58,6 +56,7 @@
       EXTENDED_GLOB
       EXTENDED_HISTORY
       HIST_EXPIRE_DUPS_FIRST
+      HIST_FIND_NO_DUPS
       HIST_FCNTL_LOCK
       HIST_IGNORE_ALL_DUPS
       HIST_IGNORE_DUPS
@@ -84,52 +83,29 @@
       FLOW_CONTROL
       EOF
 
-      # General
-      zstyle ':completion:*' file-sort modification
-      zstyle ':completion:*' rehash true
-      zstyle ':completion:*' special-dirs true
+      # Ztyle pattern
+      # :completion:<function>:<completer>:<command>:<argument>:<tag>
 
-      # Cache
+      zstyle ':completion:*' completer _complete _ignored _approximate
+      zstyle ':completion:*' complete true
+      zstyle ':completion:*' complete-options true
+      zstyle ':completion:*' file-sort modification
+      zstyle ':completion:*' group-name '''
+      zstyle ':completion:*' keep-prefix true
+      zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+      zstyle ':completion:*' menu select
+      zstyle ':completion:*' verbose true
+
+      zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+      zstyle ':completion:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+      zstyle ':completion:*:descriptions' format '%F{blue}-- %D %d --%f'
+      zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+      zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+      zstyle ':completion:*:*:-command-:*:*' group-order aliases builtins functions commands
+
       zstyle ':completion:*' use-cache on
       zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
-
-      # Completers
-      zstyle ':completion:*' completer _complete _match _approximate
-      zstyle ':completion:*:match:*' original only
-      zstyle ':completion:*:approximate:*' max-errors 1 numeric
-
-      # Complete the alias
-      zstyle ':completion:*' complete true
-
-      # Autocomplete options
-      zstyle ':completion:*' complete-options true
-
-      # Completion matching control
-      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-      zstyle ':completion:*' keep-prefix true
-
-      # Group matches
-      zstyle ':completion:*' menu select
-      zstyle ':completion:*' group-name '''
-      zstyle ':completion:*' verbose yes
-      zstyle ':completion:*:matches' group 'yes'
-      zstyle ':completion:*:options' description 'yes'
-
-      # Colors
-      zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
-
-      # Format
-      zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
-      zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
-      zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
-      zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
-
-      # Sort
-      zstyle ':completion:*' sort false
-      zstyle ':completion:*:eza' sort false
-      zstyle ':completion:*:git-checkout:*' sort false
-      zstyle ':completion:files' sort false
-      zstyle ':completion:complete:*:options' sort false
     '';
 
     shellAliases = {
