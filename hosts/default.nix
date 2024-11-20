633cb98f5f7bfeede62442d2324b8ca5e75bc7bf
raexera <raexera@gmail.com>
2024-11-20 19:24:17 +0700

    chore: update hosts

--- hosts/default.nix
+++ hosts/default.nix
@@ -3,38 +3,38 @@
   self,
   themes,
   ...
-}: let
-  nixosModules = [
-    inputs.disko.nixosModules.default
-    inputs.home-manager.nixosModules.default
-  ];
+}: {
+  flake.nixosConfigurations = let
+    nixosModules = [
+      inputs.disko.nixosModules.default
+      inputs.home-manager.nixosModules.default
+    ];
 
-  sharedModules = [
-    ./modules/config
-    ./modules/environment
-    ./modules/security
-    ./modules/system
-    ../home
-  ];
+    sharedModules = [
+      ./modules/config
+      ./modules/environment
+      ./modules/security
+      ./modules/system
+      ../home
+    ];
 
-  mkHost = name: system:
-    inputs.nixpkgs.lib.nixosSystem {
-      modules =
-        [
-          {
-            networking.hostName = name;
-            nixpkgs.hostPlatform = system;
-          }
-          ./${name}
-        ]
-        ++ nixosModules
-        ++ sharedModules;
+    mkNixosSystem = name: system:
+      inputs.nixpkgs.lib.nixosSystem {
+        modules =
+          [
+            {
+              networking.hostName = name;
+              nixpkgs.hostPlatform = system;
+            }
+            ./${name}
+          ]
+          ++ nixosModules
+          ++ sharedModules;
 
-      specialArgs = {inherit inputs self themes;};
-    };
-in {
-  flake.nixosConfigurations = {
+        specialArgs = {inherit inputs self themes;};
+      };
+  in {
     # Lenovo Yoga Slim 7 Pro X (14IAH7)
-    yuki = mkHost "yuki" "x86_64-linux";
+    yuki = mkNixosSystem "yuki" "x86_64-linux";
   };
 }
