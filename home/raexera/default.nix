be25d6510740c77ecaa6cfc4a25e5eca1443dada
raexera <raexera@gmail.com>
2024-11-23 00:26:37 +0700

    chore(hyprland): update Per-device input configs

--- home/raexera/default.nix
+++ home/raexera/default.nix
@@ -54,11 +54,17 @@
     device = let
       # Generated using https://gist.github.com/fufexan/e6bcccb7787116b8f9c31160fc8bc543
       accelpoints = "0.000 0.053 0.115 0.189 0.280 0.391 0.525 0.687 0.880 1.108 1.375 1.684 2.040 2.446 2.905 3.422 4.000 4.643 5.355 6.139";
-    in {
-      name = "pnp0c50:00-06cb:cec0-touchpad";
-      accel_profile = "custom ${accelpoints}";
-      scroll_points = accelpoints;
-      natural_scroll = true;
-    };
+    in [
+      {
+        name = "pnp0c50:00-06cb:cec0-touchpad";
+        accel_profile = "custom ${accelpoints}";
+        scroll_points = accelpoints;
+        natural_scroll = true;
+      }
+      {
+        name = "cust0000:00-27c6:0123";
+        enabled = false;
+      }
+    ];
   };
 }
