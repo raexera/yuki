7934a0f2c19cf95a070b48bfe113604623f8c93a
raexera <raexera@gmail.com>
2024-11-24 22:33:09 +0700

    chore(flake): bump inputs

--- hosts/modules/hardware/gpu/intel.nix
+++ hosts/modules/hardware/gpu/intel.nix
@@ -1,18 +1,16 @@
 {pkgs, ...}: {
-  config = {
-    boot.initrd.kernelModules = ["i915"];
+  boot.initrd.kernelModules = ["i915"];
 
-    boot.kernelParams = ["i915.enable_guc=3"];
+  boot.kernelParams = ["i915.enable_guc=3"];
 
-    hardware.graphics = {
-      extraPackages = with pkgs; [
-        intel-media-driver
-        intel-compute-runtime
-        vpl-gpu-rt
-      ];
-      extraPackages32 = with pkgs.driversi686Linux; [
-        intel-media-driver
-      ];
-    };
+  hardware.graphics = {
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-compute-runtime
+      vpl-gpu-rt
+    ];
+    extraPackages32 = with pkgs.driversi686Linux; [
+      intel-media-driver
+    ];
   };
 }
--- hosts/modules/hardware/gpu/nvidia.nix
+++ hosts/modules/hardware/gpu/nvidia.nix
@@ -16,33 +16,31 @@
 
   pCfg = config.hardware.nvidia.prime;
 in {
-  config = {
-    boot = {
-      kernelModules = [
-        "nvidia"
-        "nvidia_modeset"
-        "nvidia_drm"
-        "nvidia_uvm"
-      ];
+  boot = {
+    kernelModules = [
+      "nvidia"
+      "nvidia_modeset"
+      "nvidia_drm"
+      "nvidia_uvm"
+    ];
 
-      blacklistedKernelModules = ["nouveau"];
-    };
+    blacklistedKernelModules = ["nouveau"];
+  };
 
-    environment.variables = {
-      GBM_BACKEND = "nvidia-drm";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      NVD_BACKEND = "direct";
-    };
+  environment.variables = {
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    NVD_BACKEND = "direct";
+  };
 
-    hardware.nvidia = {
-      package = mkDefault nvidiaPackage;
-      open = mkDefault false;
-      modesetting.enable = mkDefault true;
-      powerManagement.enable = mkDefault true;
-      prime.offload = {
-        enable = mkIf (pCfg.nvidiaBusId != "" && (pCfg.intelBusId != "" || pCfg.amdgpuBusId != "")) true;
-        enableOffloadCmd = mkIf pCfg.offload.enable true;
-      };
+  hardware.nvidia = {
+    package = mkDefault nvidiaPackage;
+    open = mkDefault false;
+    modesetting.enable = mkDefault true;
+    powerManagement.enable = mkDefault true;
+    prime.offload = {
+      enable = mkIf (pCfg.nvidiaBusId != "" && (pCfg.intelBusId != "" || pCfg.amdgpuBusId != "")) true;
+      enableOffloadCmd = mkIf pCfg.offload.enable true;
     };
   };
 }
