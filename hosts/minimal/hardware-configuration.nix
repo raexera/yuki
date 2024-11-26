600441157cae68add121fa2078a525646e66b0b6
raexera <raexera@gmail.com>
2024-11-26 12:46:44 +0700

    chore(hosts): update hwconfig

--- hosts/minimal/hardware-configuration.nix
+++ hosts/minimal/hardware-configuration.nix
@@ -18,6 +18,9 @@
     };
   };
 
+  # √(3072² + 1920²) px / 14.5 in ≃ 250 dpi
+  services.xserver.dpi = 250;
+
   hardware = {
     enableRedistributableFirmware = lib.mkDefault true;
     cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
