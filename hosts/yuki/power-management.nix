380016cbdb0adcd7558d1c41c262e4850c556849
raexera <raexera@gmail.com>
2024-11-26 13:09:23 +0700

    chore(hosts): update power-management services

--- hosts/yuki/power-management.nix
+++ hosts/yuki/power-management.nix
@@ -5,32 +5,25 @@
   };
 
   powerManagement = {
-    enable = true;
     powertop.enable = true;
   };
 
   services = {
     acpid.enable = true;
 
-    auto-cpufreq = {
-      enable = true;
-      settings = {
-        charger = {
-          governor = "performance";
-          energy_performance_preference = "performance";
-          turbo = "auto";
-        };
-        battery = {
-          governor = "powersave";
-          energy_performance_preference = "power";
-          turbo = "auto";
-        };
-      };
-    };
-
     thermald.enable = true;
 
-    throttled.enable = true;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        START_CHARGE_THRESH_BAT0 = 0;
+        STOP_CHARGE_THRESH_BAT0 = 1; # battery conservation mode
+      };
+    };
 
     upower.enable = true;
   };
