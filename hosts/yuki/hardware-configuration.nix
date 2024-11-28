04eb29e036cc33987ddeead2aa8da6d34e8479c4
raexera <raexera@gmail.com>
2024-11-28 15:29:01 +0700

    chore(hosts): update modules

--- hosts/yuki/hardware-configuration.nix
+++ hosts/yuki/hardware-configuration.nix
@@ -1,4 +1,4 @@
-{
+{lib, ...}: {
   boot = {
     initrd = {
       availableKernelModules = ["nvme" "sd_mod" "thunderbolt" "usb_storage" "xhci_pci"];
@@ -14,7 +14,7 @@
     };
   };
 
-  hardware.enableRedistributableFirmware = true;
+  hardware.enableRedistributableFirmware = lib.mkDefault true;
 
   # √(3072² + 1920²) px / 14.5 in ≃ 250 dpi
   services.xserver.dpi = 250;
