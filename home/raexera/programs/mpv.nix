066ae5c6f6716fea9cd059e8382e5cc2a7524468
raexera <raexera@gmail.com>
2024-11-30 13:36:11 +0700

    chore(mpv): update config

--- home/raexera/programs/mpv.nix
+++ home/raexera/programs/mpv.nix
@@ -1,12 +1,17 @@
 {pkgs, ...}: {
   programs.mpv = {
     enable = true;
+    scripts = with pkgs.mpvScripts; [
+      mpris
+      sponsorblock
+      thumbnail
+    ];
     config = {
       gpu-context = "wayland";
       hwdec = "auto";
+      osc = "no";
       profile = "gpu-hq";
       vo = "gpu";
     };
-    scripts = with pkgs.mpvScripts; [mpris thumbnail sponsorblock];
   };
 }
